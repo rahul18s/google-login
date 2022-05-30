d00de8f92ca1a674410101fd41575771e3af2d69
Rahul Sharma <rahulsharma@Rahuls-MacBook-Pro.local>
2022-05-30 20:26:21 +0530

    google auth

--- src/App.js
+++ src/App.js
@@ -1,22 +1,71 @@
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
+import GoogleLogin from 'react-google-login';
+import {gapi} from 'gapi-script';
 import './App.css';
 
+
 function App() {
+
+  const [loginData, setLoginData] = useState(
+    localStorage.getItem('loginData')? JSON.parse(localStorage.getItem('loginData')):null
+  );
+
+  const handleFailure = (result) => {
+    console.log('result', result);
+  };
+
+  const handleLogin = async (googleData) => {
+    const res = await fetch('/api/google-login', {
+      method: 'POST',
+      body: JSON.stringify({
+        token: googleData.tokenId
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await res.json();
+
+    setLoginData(data);
+
+    localStorage.setItem('loginData', JSON.stringify(data))
+
+  };
+
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+        scope: '',
+      });
+    }
+
+    gapi.load('client:auth2', start);
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem('loginData');
+    setLoginData(null);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          loginData?<><h3>you logged in as {loginData.email}</h3>
+          <h3>Name: {loginData.name}</h3>
+          <h3>Picture: <img src={loginData.picture} /></h3>
+          <button onClick={handleLogout}>Logout</button>
+          </>: <GoogleLogin
+          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+          buttonText='LOGIN WITH GOOGLE'
+          onSuccess={handleLogin}
+          onFailure={handleFailure}
+          cookiePolicy='single_host_origin'
+        ></GoogleLogin>
+        }
+       
       </header>
     </div>
   );
